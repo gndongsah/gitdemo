854cf4e19b195e7fd1b27ed707298c4b7269937d
gndongsah <66918633+gndongsah@users.noreply.github.com>
2023-02-13 20:46:31 -0600

    Updating Index.js

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
+// importing express framework
+const express = require("express"); 
 
+const app = express(); 
 
+// Respond with "hello world" for requests that hit our root "/"
+app.get("/", function (req, res) {return res.send("Hello World");}); 
 
+// listen to port 3100 by default
+app.listen(process.env.PORT || 3100, () => {console.log("Server is running");});
 
-
-
-
-
-
-
-
-// importing express frameworkconst express = require("express"); const app = express(); // Respond with "hello world" for requests that hit our root "/"app.get("/", function (req, res) {return res.send("Hello World");}); // listen to port 3100 by defaultapp.listen(process.env.PORT || 3100, () => {console.log("Server is running");}); module.exports = app;
-/// importing express frameworkconst express = require("express"); const app = express(); // Respond with "hello world" for requests that hit our root "/"app.get("/", function (req, res) {return res.send("Hello World");}); // listen to port 3100 by defaultapp.listen(process.env.PORT || 3100, () => {console.log("Server is running");}); module.exports = app;
-///
-
-
-
-
+module.exports = app;
